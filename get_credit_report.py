d06fe30c3207c1e14b7dc5d295c686516bd7265a
Федор Мутьев <mutiev@gmail.com>
2021-04-27 17:16:15 +0600

    init

---
+++ get_credit_report.py
@@ -0,0 +1,89 @@
+# %%
+import pandas as pd
+import requests
+import json
+from zipfile import ZipFile
+import shutil
+from tqdm import tqdm
+import os
+import click
+import xmltodict
+
+requests.packages.urllib3.disable_warnings()
+
+
+def download(url: str, fname: str):
+
+    resp = requests.get(url, stream=True)
+    total = int(resp.headers.get('content-length', 0))
+    with open(fname, 'wb') as file, tqdm(
+        desc=fname,
+        total=total,
+        unit='iB',
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in resp.iter_content(chunk_size=1024):
+            size = file.write(data)
+            bar.update(size)
+
+
+with open('iin_list.txt') as f:
+    iin_list = f.read().splitlines()
+os.makedirs('tmp/', exist_ok=True)
+os.makedirs('download/', exist_ok=True)
+
+
+HEADERS = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+env_name = click.prompt(
+    'Enter environment name (test,production):', type=str, default='test')
+
+
+if os.path.exists('credentials.json'):
+    credentials = json.load(open('credentials.json'))
+    url = credentials[env_name]['url']
+    username = credentials[env_name]['username']
+    password = credentials[env_name]['password']
+else:
+    raise FileNotFoundError('credentials.json is missing')
+
+# %%
+params = {
+    "username": username,
+    "password": password,
+    "doc1_type": 14,
+    "doc1_number": "{iin}",
+    "report_type": 6,
+    "consent_confirmed": 1
+}
+
+for iin in tqdm(iin_list):
+    params['doc1_number'] = iin
+
+    resp = requests.post(url, data=json.dumps(
+        params), headers=HEADERS, verify=False)
+    if resp.status_code != 200:
+        print('\nError: status code is', resp.status_code)
+        break
+    with open('tmp/request.zip', 'wb') as f:
+        f.write(resp.content)
+
+    with ZipFile('tmp/request.zip') as myzip:
+        with myzip.open('status.xml') as xmlfile:
+            status = xmlfile.read()
+
+    status = xmltodict.parse(status)
+
+    if status['Status']['Error'] == 'True':
+        print('\n\tIIN ', iin)
+        print('\tErrorCode:', status['Status']['ErrorCode'])
+        print('\tErrorMessage', status['Status']['ErrorMessage'])
+        break
+
+    print('resp done')
+    shutil.move('tmp/request.zip', f'download/{iin}.zip')
+    break
+
+
+# %%
